fbc767522b61617d0ebe4f5f8f2e868c80e304dc
ADARSH DAROKAR <adarshdarokar55@gmail.com>
2026-05-30 22:03:32 +0530

    initial commit

---
+++ test.py
@@ -0,0 +1,35 @@
+# name = "sannnnnn"
+# print(name)
+
+# print(name[1:4])
+# print(name[1:7])
+# print(name[0:5])  
+
+
+# name = input ("enter your name")
+# print(name)
+
+
+# num = input("enter a number : ")
+# print(type(num))
+# print(int(num) +  6)
+
+
+# a1 = "scotle"
+# print("he is water type pokemon")
+
+# a2 = "scotle"
+# print("he is water type pokemon")
+
+# a3 = "scotle"
+# print("he is water type pokemon")
+
+# print(a1, a2, a3)
+
+
+
+
+ai2 = {3,4,5,5,5,5,5,56,43,3}
+
+print(ai2)
+
