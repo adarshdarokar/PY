9a72337de20155eb312c221b437250703d32017b
ADARSH DAROKAR <adarshdarokar55@gmail.com>
2026-05-30 22:44:53 +0530

    feat: dictonary implementation started

---
+++ Dictonary.py
@@ -0,0 +1,11 @@
+dict1 ={"good": "Something pleasant", "fetch": "to bring", "1": "The number 1"}
+
+marks = {"Rohit": 34, "Virat": 18, "Msdhoni": 7 }
+
+
+print (dict1["good"]);
+print (marks["Virat"]);
+
+marks['Priyanka'] = 43
+print(marks)
+
