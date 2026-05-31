a39832493d8f10c6f02b3ec5f10517289f7aa705
ADARSH DAROKAR <adarshdarokar55@gmail.com>
2026-05-31 11:49:28 +0530

    feat: dict methods implemented

--- Dictonary.py
+++ Dictonary.py
@@ -3,9 +3,17 @@ dict1 ={"good": "Something pleasant", "fetch": "to bring", "1": "The number 1"}
 marks = {"Rohit": 34, "Virat": 18, "Msdhoni": 7 }
 
 
-print (dict1["good"]);
-print (marks["Virat"]);
+# print (dict1["good"]);
+# print (marks["Virat"]);
 
-marks['Priyanka'] = 43
-print(marks)
+# marks['Priyanka'] = 43
+# print(marks)
 
+# print(marks.get("Virat"))
+# print(marks["Virat"])
+# print(marks.keys())
+# print(marks.values())
+# print(marks.items())
+
+
+# dictonary methods 
