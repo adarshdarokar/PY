0fec7140178f8629c7b36cd9577017b1ba98d10d
ADARSH DAROKAR <adarshdarokar55@gmail.com>
2026-07-04 22:35:04 +0530

    feat: add user input program to demonstrate input methods in Python

---
+++ python1/userInput.py
@@ -0,0 +1,13 @@
+print("A SIMPLE PROGRAM TO UNDERSTAND INPUT METHOD IN PYTHON")
+
+name = input ("ENTER YOUR GOOD NAME :")
+age = input ("ENTER YOUR AGE :")
+sport = input ("ENTER YOU FAV SPORT :")
+
+
+print("YOUR GOOD NAME IS :", name)
+print("YOUR AGE:", age)
+print("YOUR FAV SPORT", sport)
+
+
+
