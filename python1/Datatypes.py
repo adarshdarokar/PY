7ac502c37950cc972997a0f7a3a7edbb82208514
ADARSH DAROKAR <adarshdarokar55@gmail.com>
2026-07-06 21:28:26 +0530

    feat: add exercises for demonstrating Python data types and user input handling

---
+++ python1/Datatypes.py
@@ -0,0 +1,9 @@
+food = "protein"
+age = 21
+area = 21.70
+name = "adarsh"
+
+print(type(name))           
+print(type(age))  
+print(type(area))  
+print(type(food))           
---
+++ python1/praticle1.py
@@ -0,0 +1,12 @@
+ # program to take age as input and print value entered and its data type
+
+
+age = input("enter your: ")
+print("value entered is :", age)
+print("data type is :", type(age))
+
+
+
+age = input("enter your age :" )
+print ("this is your age :", age)
+print('data type is :', type (age)) 
